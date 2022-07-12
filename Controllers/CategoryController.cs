7f27b811c9e7a986bab1e1b4f2736c9c669c5bb1
Deivit Cholakov <azazel7298@gmail.com>
2022-07-12 13:35:58 +0300

    EF Core migrations done and view for them in list

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = this.db.Categories;
+            return View(categoryList);
+        }
+    }
+}
