3a8d844be0aaede852266c9e0f6bf5cb96767862
Deivit Cholakov <azazel7298@gmail.com>
2022-07-30 11:54:37 +0300

    Added Product Controller, Upsert action implemented , added UI for Upsert. Viewbag is used for transfering data from Product controller to the UI of Upsert.cshtml

---
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,105 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> coverTypeList = unitOfWork.Product.GetAll();
+            return View(coverTypeList);
+        }
+
+       
+
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            IEnumerable<SelectListItem> CategoryList = this.unitOfWork.Category
+                .GetAll().Select(
+                a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString(),
+
+                });
+            IEnumerable<SelectListItem> CoverTypeList = this.unitOfWork.CoverType
+                .GetAll().Select(
+                a => new SelectListItem
+                {
+                    Text = a.Name,
+                    Value = a.Id.ToString(),
+
+                });
+
+            if (id == null || id == 0)
+            {
+                ViewBag.CategoryList = CategoryList;
+                return View(product);
+            }
+
+           
+
+            return View(product);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                unitOfWork.CoverType.Update(obj);
+                unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var coverType = unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+
+            if (coverType == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverType);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+
+            if (obj == null) return NotFound();
+
+            unitOfWork.CoverType.Remove(obj);
+            unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
